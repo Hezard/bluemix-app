55fc9dda52b06ce04db422898d88e5d11681237b
akosyanenko <artur.kosyanenko@dataart.com>
2017-12-14 13:53:05 +0200

    add blumix config

---
+++ server/index.js
@@ -0,0 +1,29 @@
+var path = require('path'),
+    express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000;
+
+app.enable('trust proxy');
+
+app.use(function (req, res, next) {
+    if (req.secure || process.env.BLUEMIX_REGION === undefined) {
+      next();
+    } else {
+      console.log('redirecting to https');
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
+app.use(express.static('./dist'));
+
+app.get('/', function(request, response) {
+    response.sendFile(path.resolve('./dist/index.html'));
+});
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('listening on port: ', + port);
+});
